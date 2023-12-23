51a8ae45a5a14a31e17ba366e97ae647cc3af650
lllrikitikilll <lllrikitikilll@yandex.ru>
2023-12-23 18:12:09 +0300

    add models Item Order

--- payment_app/models.py
+++ payment_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+
+
+class Order(models.Model):
+    order_id = models.IntegerField()
+    items = models.ManyToManyField(Item)
+

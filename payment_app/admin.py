67a23ecbaca9a25d9591a4eddd722730dea1e2dd
lllrikitikilll <lllrikitikilll@yandex.ru>
2023-12-23 18:38:49 +0300

    creat admin fields

--- payment_app/admin.py
+++ payment_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.db.models import Sum
 
+from payment_app.models import Item, Order
 # Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_id', 'order_count', 'order_sum']
+
+    def order_count(self, obj):
+        return obj.items.count()
+
+    def order_sum(self, obj):
+        return obj.items.aggregate(res=Sum("price"))['res']
+
